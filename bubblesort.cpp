9b96932939c46ce6f82a61d61d421e9e85a537d0
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-05 17:38:52 +0700

    Deklarasi Variabel

---
+++ bubblesort.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; // Deklarasi array untuk menyimpan elemen-elemen yang akan diurutkan
+int n; // Deklarasi variabel untuk menyimpan jumlah elemen dalam array
