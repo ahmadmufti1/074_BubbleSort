c7bc0bd9a9840845af5eadb151b7357897699647
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-05 17:52:15 +0700

    Membuat sistem looping dengan i dimulai dai 0 hingga n-1

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -14,15 +14,20 @@ void input ()
         cout << "Masukkan jumlah elemen pada array: ";
         cin >> n;
         if (n <= 20)
-        {
             break;
         else
         {
                 cout << "Jumlah elemen tidak boleh lebih dari 20. Silakan coba lagi.\n" << endl;
         }
-        }
-        cout << endl;
-        cout << "===============" << endl;
     }
-    
+    cout << endl;
+    cout << "===============" << endl;
+    cout << "Masukkan elemen array: " << endl;
+    cout << "===============" << endl;
+
+    for (int i = 0; i < n; i++)//Looping dengan i dimulai dari 0 hingga n-1 untuk menerima input elemen array
+    {
+        cout << "Data ke-" << (i + 1) << ": "; // Menampilkan nomor data yang akan dimasukkan
+        cin >> arr[i]; // Menyimpan input elemen ke dalam array pada indeks i
+    }
 }
