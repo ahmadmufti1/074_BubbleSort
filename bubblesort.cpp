62f3b510aeec9a339975d61f5515464d8241c95e
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-05 17:44:34 +0700

    Membuat Fungsi Untuk Menerima Input dari pengguna

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -4,3 +4,23 @@ using namespace std;
 
 int arr[20]; // Deklarasi array untuk menyimpan elemen-elemen yang akan diurutkan
 int n; // Deklarasi variabel untuk menyimpan jumlah elemen dalam array
+
+void input ()
+{
+    int data;
+    while (true)
+    {
+        cout << "Masukkan jumlah elemen pada array: ";
+        cin >> n;
+        if (n <= 20)
+        {
+            break;
+            else
+            {
+                cout << "Jumlah elemen tidak boleh lebih dari 20. Silakan coba lagi.\n" << endl;
+            }
+        }
+        cout << endl;
+    }
+    
+}
